b890cfa101857132e3e6aba747944f45d339fd13
Timbo997 <brainy.elephant@gmail.com>
2017-02-20 21:12:27 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
 public void battle(Ally ally,Enemy enemy){
-    
+  System.out.print(enemy + " attacked!\n" + ally + " is ready to fight");
+  for(enemy.currentHP = enemy.maxHP, enemy.currentHP = 0 | ally.currentHP = 0,)  
   }
 public static void main(String[] args) {
     
